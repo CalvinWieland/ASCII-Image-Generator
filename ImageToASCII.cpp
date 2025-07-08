fbe8db3805ddf9c81a0a8d61e4c7d9f9a2a19db9
Calvin Wieland <calvinwieland@yahoo.com>
2025-07-07 21:01:22 -0700

    Refactor code and allow for command line arguments

--- ImageToASCII.cpp
+++ ImageToASCII.cpp
@@ -1,5 +1,7 @@
+#include <cstdlib>
 #include <stdio.h>
 #include <iostream>
+#include <stdlib.h>
 
 #define _USE_MATH_DEFINES
 #include <math.h>
@@ -14,18 +16,17 @@
 #pragma clang diagnostic pop
 
 char characters[70] = "@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+char charactersReversed[70] = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@";
 
 // boxHeight and boxWidth specify the number of pixels in the height and width of the box that an ASCII character will
 // replace. Since chars are twice as tall as they are wide, it's best to set the height to twice the size of the width
-int boxWidth = 3;
-int boxHeight = 6;
-const char* inputFileName = "lizardsmaller.jpg";
-const char* outputFileName = "lizard gaussian.jpg";
-const char* outputFileName1 = "lizard sobel X.jpg";
-const char* outputFileName2 = "lizard sobel Y.jpg";
-const char* outputFileName3 = "lizard gradient G.jpg";
-const char* outputFileName4 = "lizard NMS.jpg";
-const char* outputFileName5 = "lizard double threshold.jpg";
+
+const char* outputFileName = "gaussian.jpg";
+const char* outputFileName1 = "sobel X.jpg";
+const char* outputFileName2 = "sobel Y.jpg";
+const char* outputFileName3 = "gradient G.jpg";
+const char* outputFileName4 = "NMS.jpg";
+const char* outputFileName5 = "double threshold and pixelation.jpg";
 
 // Grayscales the image based on image's pixel data, height, width, and channels
 void GrayScaleImage(unsigned char* data, int height, int width, int channels) {
@@ -47,7 +48,7 @@ void GrayScaleImage(unsigned char* data, int height, int width, int channels) {
 
 // Takes image pixel data, a 2D array ASCIIArt, image height and width, and channel count
 // and converts the image into ASCII art
-void ImageToASCII(unsigned char* data, char ASCIIArt[], int height, int width, int channels) {
+void PixelDataToASCII(unsigned char* data, char ASCIIArt[], int height, int width, int channels, int boxHeight, int boxWidth) {
     for (int row = 0; row <= height - boxHeight; row += boxHeight) {
         for (int col = 0; col <= width - boxWidth; col += boxWidth) {
             int avg = 0;
@@ -69,7 +70,7 @@ void ImageToASCII(unsigned char* data, char ASCIIArt[], int height, int width, i
             //std::cout << "Character index: " << (int)(avg * (((double)68 / 255)/1.3)) << std::endl;
             //std::cout << "Character to be printed: " << characters[(int)(avg * (((double)68 / 255)/1.3))] << std::endl;
 
-            ASCIIArt[(row / boxHeight) * (width / boxWidth) + col / boxWidth] = characters[(int)(avg * ((double)68 / 255))];
+            ASCIIArt[(row / boxHeight) * (width / boxWidth) + col / boxWidth] = charactersReversed[(int)(avg * ((double)68 / 255))];
 
             // Make each value in the box equal to the average
             for (int i = row; i < row + boxHeight; i++) {
@@ -88,7 +89,7 @@ void ImageToASCII(unsigned char* data, char ASCIIArt[], int height, int width, i
 
 // Takes ASCIIArt array and the height and width of the original image
 // and uses them and the boxHeight and boxWidth to print the array
-void PrintASCIIArt(char ASCIIArt[], int height, int width) {
+void PrintASCIIArt(char ASCIIArt[], int height, int width, int boxHeight, int boxWidth) {
     // Print the ASCII art
     for (int i = 0; i < (height / boxHeight) * (width / boxWidth); i++) {
         std::cout << ASCIIArt[i];
@@ -223,40 +224,48 @@ void NonMaximumSupression(unsigned char* G, unsigned char* NMS, int height, int 
             int gradientIndex = (row * width + col) * channels;
             int angleInfoIndex = row * width + col;
 
+            // If the angle at that pixel is 0
             if (angleInfo[angleInfoIndex] == 0) {
                 int neighbor1 = (row * width + (col + 1)) * channels;
                 int neighbor2 = (row * width + (col - 1)) * channels;
 
+                // If pixel is larger than those to left and right, count them in NMS
                 if (G[gradientIndex] > G[neighbor1] && G[gradientIndex] > G[neighbor2]) {
                     NMS[gradientIndex + 0] = G[gradientIndex + 0];
                     NMS[gradientIndex + 1] = G[gradientIndex + 1];
                     NMS[gradientIndex + 2] = G[gradientIndex + 2];
                 }
             }
+            // If the angle at that pixel is 45
             else if (angleInfo[angleInfoIndex] == 45) {
                 int neighbor1 = ((row - 1) * width + (col + 1)) * channels;
                 int neighbor2 = ((row + 1) * width + (col - 1)) * channels;
 
+                // If pixel is larger than those to top-right and bottom-right, count them in NMS
                 if (G[gradientIndex] > G[neighbor1] && G[gradientIndex] > G[neighbor2]) {
                     NMS[gradientIndex + 0] = G[gradientIndex + 0];
                     NMS[gradientIndex + 1] = G[gradientIndex + 1];
                     NMS[gradientIndex + 2] = G[gradientIndex + 2];
                 }
             }
+            // If the angle at that pixel is 90
             else if (angleInfo[angleInfoIndex] == 90) {
                 int neighbor1 = ((row + 1) * width) * channels;
                 int neighbor2 = ((row - 1) * width) * channels;
 
+                // If pixel is larger than those above and below it, count them in NMS
                 if (G[gradientIndex] > G[neighbor1] && G[gradientIndex] > G[neighbor2]) {
                     NMS[gradientIndex + 0] = G[gradientIndex + 0];
                     NMS[gradientIndex + 1] = G[gradientIndex + 1];
                     NMS[gradientIndex + 2] = G[gradientIndex + 2];
                 }
             }
+            // If the angle at that pixel is 135
             else {
                 int neighbor1 = ((row + 1) * width + (col + 1)) * channels;
                 int neighbor2 = ((row - 1) * width + (col - 1)) * channels;
 
+                // If pixel is larger than those to top-left and bottom-right, count them in NMS
                 if (G[gradientIndex] > G[neighbor1] && G[gradientIndex] > G[neighbor2]) {
                     NMS[gradientIndex + 0] = G[gradientIndex + 0];
                     NMS[gradientIndex + 1] = G[gradientIndex + 1];
@@ -305,7 +314,8 @@ void DoubleThresholdAndHysteresis(unsigned char* original, unsigned char* result
     }
 }
 
-int main() {
+// Takes an image file path and returns (?)
+char* ImageToASCII(const char* inputFileName, int boxHeight, int boxWidth, double doubleThresholdMin, double doubleThresholdMax) {
     int width, height, channels;
 
     // Loads image and stores pixel info in the data char array
@@ -313,10 +323,11 @@ int main() {
 
     if (!data) {
         std::cerr << "Loading image failed\n";
-        return 1;
+        return NULL;
     }
 
-
+    //char ASCIIArt[(height / boxHeight) * (width / boxWidth)];
+    char* ASCIIArt = (char *)malloc((height / boxHeight) * (width / boxWidth));
     int* SobelX = (int*)malloc(sizeof(int) * width * height * channels);
     int* SobelY = (int*)malloc(sizeof(int) * width * height * channels);
 
@@ -339,8 +350,12 @@ int main() {
     NonMaximumSupression(G, NMS, height, width, channels, angleInfo);
 
     // set doubleThreshold values
-    double min = .5, max = .8;
+    double min = .3, max = .5;
     DoubleThresholdAndHysteresis(NMS, doubleThreshold, min, max, height, width, channels);
+
+    PixelDataToASCII(doubleThreshold, ASCIIArt, height, width, channels, boxHeight, boxWidth);
+
+    PrintASCIIArt(ASCIIArt, height, width, boxHeight, boxWidth);
     
     stbi_write_jpg(outputFileName, width, height, channels, data, 100);
     stbi_write_jpg(outputFileName1, width, height, channels, SobelX, 100);
@@ -351,10 +366,41 @@ int main() {
 
     // Free memory allocated for data
     stbi_image_free(data);
-    stbi_image_free(SobelX);
-    stbi_image_free(SobelY);
+    free(SobelX);
+    free(SobelY);
     free(angleInfo);
     free(G);
+    free(NMS);
+    free(doubleThreshold);
+
+    return NULL;
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 6) {
+        std::cout << "Not enough arguments.\nArguments: \n  1. Path to your image\n  (2.) pixel height and (3.) width of each ASCII character";
+        std::cout << " (meaning, your image will be split into blocks of height x width \n  blocks and one character will represent each block)";
+        std::cout << ". It's recommended that the width is approximately half of the height,\n  since ASCII character slots are twice as tall as they are wide.";
+        std::cout << "  A good default is a width of 6 and a height of 12.\n";
+        std::cout << "  (4.) The low and (5.) high values of the double threshold step. The lower the values are, the more edges there will be,\n";
+        std::cout << "  and the higher they are, the more strict the edge detection algorithm will be, meaning less edges.\n  Good defaults are low = .3 and high = .5\n";
+        std::cout << "Sample input: ./ImageToASCII input.png 12 6 .3 .5";
+
+        return 0;
+    }
+    const char* inputFileName = argv[1];
+    //inputFileName = "./images/inspiration.png";
+
+    int boxHeight = atoi(argv[2]);
+    int boxWidth = atoi(argv[3]);
+    double low = strtod(argv[4], NULL);
+    double high = strtod(argv[5], NULL);
+
+    //boxHeight = 6, boxWidth = 3;
+    //low = .3, high = .5;
+
+    ImageToASCII(inputFileName, boxHeight, boxWidth, low, high);
 
     return 0;
 }
